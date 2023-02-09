55ecc686030dc1dcaeea46bcb14b9c8dd6e8ffe7
Daniel Suaza <daniel.suaza@mail.escuelainf.edu.co>
2023-02-09 15:23:58 -0500

    compila ej1

---
+++ ej1/build/CMakeFiles/ej1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ej1.dir/ej1.c.o"
+  "CMakeFiles/ej1.dir/ej1.c.o.d"
+  "ej1"
+  "ej1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ej1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
