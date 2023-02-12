c7b3b4f6706a55270f9d0e5a7694f200ff47bc83
Mariana Muñoz Parra <mariana.munoz@mail.escuelaing.edu.co>
2023-02-11 22:00:24 -0500

    Avance 1 Promedio arreglo

---
+++ ej3/Promedio.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+float arreglo[10];
+float s;
+float prom;
+s = 0;
+cout <<"Ingresar 10 números para obtener su promedio. \n";
+
+for (int i=0; i<=9;i=i+1){
+cout <<"Ingrese un número:";
+cin >> arreglo[i];
+s = s + arreglo[i];
+}
+
+prom = s/10;
+cout<<"El promedio de los números ingresados es:"<<prom<<"\n";
+return 0;
+}
