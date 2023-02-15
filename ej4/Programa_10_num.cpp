cbe720d7c977d9b7f45ec0b18733f43309aaabeb
David-S-M-F <david.munoz-f@mail.escuelaing.edu.co>
2023-02-15 15:18:04 -0500

    Version para pull request

--- ej4/Programa_10_num.cpp
+++ ej4/Programa_10_num.cpp
@@ -12,7 +12,9 @@ int main ()
     for (pos=0; pos<10; pos++){
         cin >> n;
         vec[pos] = n;
-        suma = suma + n;
+    }
+    for (i=0; i<10; i++){
+	suma = suma + vec[i];
     }
     prom = suma/10;
     cout << "El promedio es " << prom << endl;
