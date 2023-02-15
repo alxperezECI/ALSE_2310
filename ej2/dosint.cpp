02f8d638fa5ab30a4e73d0538c9849984a1b45ee
Daniel Suaza <daniel.suaza@mail.escuelaing.edu.co>
2023-02-15 15:27:36 -0500

    suma del vector y promedio

--- ej2/dosint.cpp
+++ ej2/dosint.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(){
-	float vec[10];
+	int vec[10];
 	float suma=0;
 	float prom = 0.0;
 	cout << "Ingrese 10 valores enteros: \n";
@@ -12,8 +12,8 @@ int main(){
 	}
 	for (int vc = 0; vc < 10; vc++){
 		suma = vec[vc] + suma;
-		
 	}
 	prom = suma/10;
 	cout << "La suma es: " << suma << "\n"<<"El promedio es: "<< prom << "\n";
+	return 0;
 }
