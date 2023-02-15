574469d37e09720bfee95fa985371cc45a2335a3
Jhonatan3011 <jhonatan.tellez@mail.escuelaing.edu.co>
2023-02-15 15:27:24 -0500

    ejercicio promedio con 10 numeros

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -3,16 +3,17 @@ using namespace std;
 
 int main ()
 {
-  float num=0, sum=0;
+  int n[10];
+  float sum;
  cout << "Te pedire 10 numeros: "<< endl;
  for (int i=0; i<=9; i++)
  {
   
    cout << "Ingrese el numero "<< i+1 << " ";
-   cin >> num;
-  sum=sum+num;
+   cin >> n[i];
+   sum = sum + n[i];
  }
- sum=sum/10;
+ sum=sum/10.;
  cout << "el resultado es " << sum << endl;
 return 0;
 }
