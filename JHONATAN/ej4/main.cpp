341a6e3a59de3c171bc3ba65f3cf1b080b5c5122
Jhonatan3011 <jhonatan.tellez@mail.escuelaing.edu.co>
2023-02-16 16:09:16 -0500

    Mediana para 10 datos

--- JHONATAN/ej4/main.cpp
+++ JHONATAN/ej4/main.cpp
@@ -1,29 +1,27 @@
-#include <iostream>
+	#include <iostream>
 
 using namespace std;
 
-float promedio( int v[] ){
-  float prom = 0.;
+float mediana( int v[] ){
+  float med = 0.;
   
-  for( int i = 0; i < 10; i++){
-    prom += v[i];
-  }
-  
-  return prom / 10.;
+  med = (v[4]+v[5])/2.;
+  return med;
 }
 
 int main(){
   int vec[10];
-  float prom=0.;
+  float med=0.;
 
-  cout << "Ingresar diez (10) número enteros: ";
+  cout << "Ingresar diez (10) número enteros: " << endl;
 
   for( int i = 0; i < 10; i++){
+    cout << "Ingrese el numero " << i+1 << ":";
     cin >> vec[i];
   }
 
-  prom = promedio( vec );
+  med = mediana( vec );
 
-  cout << "El promedio de los datos ingresados es: " << prom << "\n";
+  cout << "La mediana de los datos ingresados es: " << med << "\n";
   return 0;
 }
