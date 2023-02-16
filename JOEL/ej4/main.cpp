0aa043a0cc9b0f5206e25ace9d4e55919d508e28
Joel S. Hernández <joel.hernandez-c@mail.escuelaing.edu.co>
2023-02-16 16:06:46 -0500

    Añadidas funciones mediana y printvec. Ajustado el cógigo para emplear funciones.

--- JOEL/ej4/main.cpp
+++ JOEL/ej4/main.cpp
@@ -2,28 +2,116 @@
 
 using namespace std;
 
-float promedio( int v[] ){
-  float prom = 0.;
-  
-  for( int i = 0; i < 10; i++){
-    prom += v[i];
-  }
-  
-  return prom / 10.;
+//Funciones:
+// 1. float maximos(int vec[])
+// 2. float minimos(int vec[])
+// 3. float mediana(int vec[]) [√]
+// 4. float suma(int vec[])
+// 5. float promedio(int vec[])
+
+float promedio( int v[], float len ){
+   float prom = 0.;
+
+   for( int i = 0; i < len; i++){
+     prom += v[i];
+   }
+
+   return prom / len;
+ }
+
+int printvec(int vec[], int len, string msg)
+{
+    // flush(cout);
+    cout<<"\n"<<msg<<endl;
+    for(int i=0;i<len;i++)
+    {
+        cout<<vec[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
 }
 
+float mediana(int vec[],int len, bool imprimir_ordenado)
+{
+    int v_org[len]; 
+    int aux;
+
+    // Copiar vec en v_org
+    for(int i=0;i<len;i++)
+    {
+        v_org[i]=vec[i];
+    }
+    
+    // Ordenar el vector
+    for(int j=0; j<len;j++)
+    {
+        for(int i=0;i<len;i++)
+        {
+            if(i+1==len)
+                break;
+            else
+            {
+                if(v_org[i]>v_org[i+1])
+                {
+                    aux=v_org[i];
+                    v_org[i]=v_org[i+1];
+                    v_org[i+1]=aux;
+                }
+                else
+                {
+                    continue;  
+                }      
+            }
+        }
+    }
+
+    // Imprimir vector ordenado
+    if(imprimir_ordenado)
+    {
+        printvec(v_org,len,"Vector ordenado: ");
+    }
+    // Calcular mediana
+
+    if (len%2==0)
+    {
+        return (vec[len/2-1]+vec[len/2])/2.0;
+    }
+    else
+    {
+        return vec[len/2];
+    }
+
+
+    
+    // return vec;
+}
+
+
+
 int main(){
+
   int vec[10];
   float prom=0.;
+  float med;
+  int len=10;
 
-  cout << "Ingresar diez (10) número enteros: ";
+  cout << "Ingresar diez ("<<len<<") número enteros:\n";
 
   for( int i = 0; i < 10; i++){
+   
+    cout<<"Numero "<<i+1<<": ";
     cin >> vec[i];
   }
 
-  prom = promedio( vec );
+  printvec(vec,len,"Vector original: ");
 
-  cout << "El promedio de los datos ingresados es: " << prom << "\n";
+  prom = promedio( vec,len );
+
+  cout << "\nEl promedio de los datos ingresados es: " << prom << "\n";
+
+  med=mediana(vec,len,true);
+
+  cout << "\nLa mediana de los datos ingresados es: " << med << "\n";
+  
   return 0;
 }
