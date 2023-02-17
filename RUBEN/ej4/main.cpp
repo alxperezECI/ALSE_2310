b513978d6552dffddc890ac3fae9be383141856d
RubenC7 <63657465+RubenC7@users.noreply.github.com>
2023-02-17 13:37:13 -0500

    Update main.cpp

--- RUBEN/ej4/main.cpp
+++ RUBEN/ej4/main.cpp
@@ -2,28 +2,36 @@
 
 using namespace std;
 
-float promedio( int v[] ){
-  float prom = 0.;
-  
-  for( int i = 0; i < 10; i++){
-    prom += v[i];
-  }
-  
-  return prom / 10.;
+float promedio(int x[], int tam);
+
+int main()
+{
+    int tam = 10, numeros[tam];
+
+    
+    cout<<"\n .Digite 10 numeros enteros: " << endl;
+
+    for(int i=0; i <tam ; i++)
+        {
+            cout<<"\n Digite un numero entero: " << endl;
+            cin >>numeros[i];
+        }
+        
+        
+    cout<<"\n El promedio de los numeros ingresados es: " << promedio(numeros, tam) << endl;
+        
+        
+        
+    return 0;
 }
 
-int main(){
-  int vec[10];
-  float prom=0.;
 
-  cout << "Ingresar diez (10) número enteros: ";
-
-  for( int i = 0; i < 10; i++){
-    cin >> vec[i];
-  }
-
-  prom = promedio( vec );
-
-  cout << "El promedio de los datos ingresados es: " << prom << "\n";
-  return 0;
+float promedio(int x[], int tam)
+{
+    float suma;
+    
+    for(int i=0; i <=tam ; i++)
+            suma = suma + x[i];
+            
+    return suma/tam;
 }
