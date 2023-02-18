2b369af1e5e783fda5ef83a7bb780d1b69101d2a
Joel S. Hernández <joel.hernandez-c@mail.escuelaing.edu.co>
2023-02-17 21:12:22 -0500

    Añadido: Usar argumentos de main como vector de entrada. Retornar error si no recibe len (10) argumentos.

--- JOEL/ej6/main.cpp
+++ JOEL/ej6/main.cpp
@@ -11,6 +11,7 @@ float promedio( int v[], int cant ){
   
   return prom / (float)cant;
 }
+
 /*
 float promedio( float v[], int cant ){
   float prom = 0.;
@@ -25,17 +26,24 @@ float promedio( float v[], int cant ){
 
 
 int main(int argc, char** argv){
+
+  int len=10;
+  
+  if(argc!=len){
+    cout << "Cantidad de argumentos incorrecta" << endl;
+    return 1;
+  }
+
   cout << "Cantidad de argumentos: " << argc << endl;
 
   for(int i=0; i< argc; i++)  
     cout << argv[i] << endl;
 
-
   int vec[10];
   float prom=0.;
 
   // Creación de un puntero
-
+  /*
   float* dirProm;
 
   cout << "Vector creado en dirección: " << vec << endl;
@@ -44,9 +52,12 @@ int main(int argc, char** argv){
   dirProm = &prom;
   cout << "Dirección de prom: " << dirProm << " almacenado en: " << &dirProm << endl;
   cout << "Ingresar diez (10) número enteros: ";
+  */
 
-  for( int i = 0; i < 10; i++){
-    cin >> vec[i];
+  // leer argumentos de main en vec y convertirlos a enteros
+  for( int i = 1; i < 10; i++){
+    // cin >> vec[i];
+    vec[i]=stoi(argv[i]);
   }
 
   prom = promedio( vec, 10 );
