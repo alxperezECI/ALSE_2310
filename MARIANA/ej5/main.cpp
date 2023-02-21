25f0eb55fb0e4f6f117673d4436451cbf06b5f87
Mariana Muñoz Parra <mariana.munoz@mail.escuelaing.edu.co>
2023-02-20 20:36:23 -0500

    Ej5 y Ej6

---
+++ MARIANA/ej5/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+float mediana(int Vec[]){
+  float med;
+  
+  med = (Vec[4]+Vec[5])/2.;
+  
+  return med;
+}
+
+int main(){
+  int arreglo[10];
+  float med=0.;
+
+//Creación Puntero
+  float *dirmed;
+  dirmed = &med;
+  cout << "Dirección de med: " << dirmed << " almacenado en: " << &dirmed << endl;
+
+  cout << "Ingresar diez números enteros para obtener su mediana: \n ";
+ 
+  for( int i = 0; i < 10; i++){
+    cout << "Ingrese el numero " << i+1 << ":";
+    cin >> arreglo[i];
+  }
+
+  med = mediana(arreglo);
+
+  cout << "La mediana de los datos ingresados es: " << med << "\n";
+  return 0;
+}
