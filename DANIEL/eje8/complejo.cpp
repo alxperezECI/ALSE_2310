20d9d14dfeb50f240220ca7d381ebecb00a1a84f
Daniel Suaza <daniel.suaza@mail.escuelaing.edu.co>
2023-03-02 13:46:34 -0500

    Entrega funciones

---
+++ DANIEL/eje8/complejo.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <math.h>
+#include "complejo.h"
+
+using namespace std;
+
+bool Complejo::setRe( double r ){ 
+  re = r; 
+  return true;
+}
+
+bool Complejo::setIm( double i ){ 
+  im = i; 
+  return true;
+}
+
+double Complejo::distancia( const Complejo &b ){
+    double x = b.re - re;
+    double y = b.im - im;
+    return sqrt( x * x  + y * y );
+  }
+
+Complejo Complejo::operator+(Complejo &a){
+    Complejo c;
+    c.re = re + a.re;
+    c.im = im + a.im;
+    return c;
+  }
+
+ostream& operator<< (ostream& out, Complejo &a){
+    out  << a.re << " + " << a.im << "j";
+    return out;
+  }
+  
+
+double   Complejo::getRe(){
+  return re;
+}
+
+double    Complejo::getIm(){
+  return im;
+}
+
+Complejo  Complejo::operator-(Complejo &a){
+  Complejo c;
+  return c;
+}
+
+/*Complejo  Complejo::operator*(Complejo &a){
+  Complejo c;
+  return c;
+}*/
+
+Complejo  Complejo::conjugado(){
+  Complejo c;
+  c.re = re;
+  c.im = (-1)*im;
+  return c;
+}
+
+Complejo  Complejo::inverso(const Complejo b){
+  Complejo c;
+  float magnitud = 0;
+  magnitud = (re*re)+(im+im)
+  c.re = b.re/magnitud;
+  c.im = b.im/magnitud;
+  //b.inverso(b.conjugado(b));
+  return c;
+}
