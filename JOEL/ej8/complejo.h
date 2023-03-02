7a753db250fb6e153d1ed7d21d2e2354b460c85e
Joel S. Hernández <joel.hernandez-c@mail.escuelaing.edu.co>
2023-03-01 22:07:58 -0500

    complejo.h: actualizado formato.

--- JOEL/ej8/complejo.h
+++ JOEL/ej8/complejo.h
@@ -1,21 +1,22 @@
 #include <iostream>
 #include <math.h>
+
 using namespace std;
 
-class Complejo{
-  double re,im;
+class Complejo
+{
+  double re, im;
 
 public:
-  double   getRe();
-  bool     setRe( double r );
-  double   getIm();
-  bool     setIm( double i );
-  double   distancia( const Complejo &b );
+  double getRe();
+  bool setRe(double r);
+  double getIm();
+  bool setIm(double i);
+  double distancia(const Complejo &b);
   Complejo operator+(Complejo &a);
   Complejo operator-(Complejo &a);
   Complejo operator*(Complejo &a);
   Complejo conjugado();
   Complejo inverso();
-  friend   ostream& operator<< (ostream& out, Complejo &a);
+  friend ostream &operator<<(ostream &out, Complejo &a);
 };
-
